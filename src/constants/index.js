8fe7a600a396190f22e643971c05f92e118156a7
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-21 10:20:10 +0530

    constants for list, text etc

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+import adobeLogo from "../assets/brands/adobe.png";
+import airtableLogo from "../assets/brands/airtable.png";
+import elasticLogo from "../assets/brands/elastic.png";
+import framerLogo from "../assets/brands/framer.png";
+import opendoorLogo from "../assets/brands/opendoor.png";
+
+export const BRANDS = [
+	{ name: "Adobe", logo: adobeLogo },
+	{ name: "Elastic", logo: elasticLogo },
+	{ name: "OpenDoor", logo: opendoorLogo },
+	{ name: "Airtable", logo: airtableLogo },
+	{ name: "Elastic", logo: elasticLogo },
+	{ name: "Framer", logo: framerLogo },
+];
