e7811637b672512b7bd3c869196cc859ca9f8e38
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-22 19:27:58 +0530

    route added for login, signup. media queries for signup and login page

--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -1,16 +1,21 @@
 import Logo from "./Logo";
 import { Button } from "../ui";
 import styles from "./Header.module.css";
+import { Link } from "react-router";
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
 			<Logo />
 			<div className={styles.actions}>
-				<Button size="small" isLink={true}>
-					Login
-				</Button>
-				<Button size="small">Sign Up</Button>
+				<Link to="/login">
+					<Button size="small" isLink={true}>
+						Login
+					</Button>
+				</Link>
+				<Link to="/signup">
+					<Button size="small">Sign Up</Button>
+				</Link>
 			</div>
 		</header>
 	);
