be494263dcf9bf31e078f115609e7123c3bda80a
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-22 22:55:52 +0530

    router setup for dashboard

--- src/router/Layout.jsx
+++ src/router/Layout.jsx
@@ -1,7 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Landing from "../pages/Landing";
-import Login from "../pages/Login";
-import Signup from "../pages/Signup";
+import { Landing, Login, Signup, Admin } from "../pages";
+import {
+	Home,
+	ContactCenter,
+	Analytics,
+	Bots,
+	Team,
+	Setting,
+} from "../components/admin/dashboard";
 export default function Layout() {
 	return (
 		<BrowserRouter>
@@ -9,6 +15,14 @@ export default function Layout() {
 				<Route path="/" element={<Landing />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
+				<Route path="/admin" element={<Admin />}>
+					<Route path="dashboard" element={<Home />} />
+					<Route path="chats" element={<ContactCenter />} />
+					<Route path="analytics" element={<Analytics />} />
+					<Route path="bots" element={<Bots />} />
+					<Route path="team" element={<Team />} />
+					<Route path="setting" element={<Setting />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
