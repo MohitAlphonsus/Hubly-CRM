6088ed6d6936f314a47d9f0acdc4a6de1a6b255f
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-23 11:08:09 +0530

    active link state management

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -2,16 +2,22 @@ import { DASHBOARD_LINKS } from "../../constants";
 import dashboardLogoIcon from "../../assets/icons/d-logo-icon.svg";
 import styles from "./Sidebar.module.css";
 import { NavLink } from "react-router";
+import { useState } from "react";
 
 export default function Sidebar() {
+	const [activeLink, setActiveLink] = useState();
 	return (
 		<aside className={styles.sidebar}>
 			<img src={dashboardLogoIcon} alt="hubly-icon-logo" />
 			<nav>
 				<ul>
-					{DASHBOARD_LINKS.map(({ id, icon, title, route }) => (
+					{DASHBOARD_LINKS.map(({ id, icon, title, route }, index) => (
 						<li key={id}>
-							<NavLink to={route}>
+							<NavLink
+								to={route}
+								className={({ isActive }) => (isActive ? styles.active : "")}
+								onClick={() => setActiveLink(index)}
+							>
 								<img src={icon} alt={title} />
 								<span>{title}</span>
 							</NavLink>
