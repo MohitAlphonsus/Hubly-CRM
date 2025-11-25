598cfbe1075b764f10d2b8c79d1dbc8088af8434
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-25 21:20:32 +0530

    logout button added on setting page, AuthProvider added on App page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Layout from "./router/Layout";
-
+import { TeamAuthProvider } from "./context/teamAuthContext";
 export default function App() {
 	return (
-		<>
+		<TeamAuthProvider>
 			<Layout />
-		</>
+		</TeamAuthProvider>
 	);
 }
--- src/components/admin/dashboard/Setting.jsx
+++ src/components/admin/dashboard/Setting.jsx
@@ -1,5 +1,10 @@
+import { Button } from "../../ui";
 import styles from "./Setting.module.css";
+import { useTeamAuth } from '../../../hooks/useTeamAuth'
 
 export default function Setting() {
-	return <div className={styles.setting}>Setting</div>;
+	const { logout } = useTeamAuth();
+	return <div className={styles.setting}>
+		<Button size="small" variant="primary" className={styles["sign-out-btn"]} onClick={logout}>Log Out</Button>
+	</div>;
 }
