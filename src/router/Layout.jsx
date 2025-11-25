c1c7519b6dd9ec4435a8cb82ee89d07fbe20cbc4
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-25 21:17:11 +0530

    routes layout based on authentication

--- src/router/Layout.jsx
+++ src/router/Layout.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { Landing, Login, Signup, Admin } from "../pages";
 import {
 	Home,
@@ -8,21 +8,43 @@ import {
 	Team,
 	Setting,
 } from "../components/admin/dashboard";
+import { useTeamAuth } from "../hooks/useTeamAuth";
+
 export default function Layout() {
+	const { currentTeamMember } = useTeamAuth();
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Landing />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
-				<Route path="/admin" element={<Admin />}>
-					<Route path="dashboard" element={<Home />} />
-					<Route path="chats" element={<ContactCenter />} />
-					<Route path="analytics" element={<Analytics />} />
-					<Route path="bots" element={<Bots />} />
-					<Route path="team" element={<Team />} />
-					<Route path="setting" element={<Setting />} />
-				</Route>
+				{!currentTeamMember && (
+					<>
+						<Route path="/" element={<Landing />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/admin/*" element={<Navigate to="/login" />} />
+					</>
+				)}
+
+				{currentTeamMember && (
+					<>
+						<Route path="/" element={<Navigate to="/admin/dashboard" />} />
+						<Route path="/login" element={<Navigate to="/admin/dashboard" />} />
+						<Route path="/signup" element={<Navigate to="/admin/dashboard" />} />
+
+						<Route path="/admin" element={<Navigate to="/admin/dashboard" />} replace />
+
+						<Route path="/admin" element={<Admin />}>
+							<Route path="dashboard" index element={<Home />} />
+							<Route path="chats" element={<ContactCenter />} />
+							<Route path="analytics" element={<Analytics />} />
+							<Route path="bots" element={<Bots />} />
+							<Route path="team" element={<Team />} />
+							<Route path="setting" element={<Setting />} />
+						</Route>
+					</>
+				)}
+
+				<Route path="*" element={<Navigate to={currentTeamMember ? "/admin/dashboard" : "/"} replace />} />
+
 			</Routes>
 		</BrowserRouter>
 	);
