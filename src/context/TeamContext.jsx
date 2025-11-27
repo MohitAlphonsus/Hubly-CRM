5a01807af3c9111f82f5a8fa3476d5cff1607805
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-28 00:54:53 +0530

    context for team related api routes function handling

--- src/context/TeamContext.jsx
+++ src/context/TeamContext.jsx
@@ -1,9 +1,25 @@
 import { createContext, useState } from "react";
+import {
+	addTeamMember,
+	deleteTeamMember,
+	editAndUpdateTeamMember,
+	getAllTeamMembers,
+} from "../api/teamApi";
 
 const TeamContext = createContext();
 
 function TeamProvider({ children }) {
-	return <TeamContext.Provider value={{}}>{children}</TeamContext.Provider>;
+	const [team, setTeam] = useState([]);
+
+	const handleFetchAllTeamMembers = async () => {
+		const response = await getAllTeamMembers();
+		setTeam(response.data.teamMembers);
+	};
+	return (
+		<TeamContext.Provider value={{ team, handleFetchAllTeamMembers }}>
+			{children}
+		</TeamContext.Provider>
+	);
 }
 
 export { TeamProvider, TeamContext };
