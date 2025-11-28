20c37214860b9291ebbb2d3e8cce1be5488f887b
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-29 01:26:26 +0530

    edit form added on settings page

--- src/components/admin/dashboard/Setting.jsx
+++ src/components/admin/dashboard/Setting.jsx
@@ -1,10 +1,24 @@
 import { Button } from "../../ui";
-import styles from "./Setting.module.css";
-import { useTeamAuth } from '../../../hooks/useTeamAuth'
+import { useTeamAuth } from "../../../hooks/useTeamAuth";
+import TeamForm from "./d-ui/TeamForm";
 
+import styles from "./Setting.module.css";
 export default function Setting() {
 	const { logout } = useTeamAuth();
-	return <div className={styles.setting}>
-		<Button size="small" variant="primary" className={styles["sign-out-btn"]} onClick={logout}>Log Out</Button>
-	</div>;
+	return (
+		<div className={styles.setting}>
+			<Button
+				size="small"
+				variant="primary"
+				className={styles["sign-out-btn"]}
+				onClick={logout}
+			>
+				Log Out
+			</Button>
+			<div className={styles.edit__form__container}>
+				<h4>Edit Profile</h4>
+				<TeamForm />
+			</div>
+		</div>
+	);
 }
