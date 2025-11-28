8a6e5db4cc1fccdc7faa7040790bf3a144c2a9a8
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-29 01:30:32 +0530

    component modified to make more dynamic

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,9 +1,37 @@
 import FormGroup from "./FormGroup";
 
-export default function Input({ type, label, name, placeholder = "", onChange, value, checked = false }) {
+export default function Input({
+	type,
+	label,
+	name,
+	placeholder = "",
+	onChange,
+	value,
+	checked = false,
+	className = undefined,
+}) {
 	return (
 		<FormGroup label={label}>
-			{type === "checkbox" ? <input type={type} id={label} name={name} placeholder={placeholder} onChange={onChange} value={value} checked={checked} /> : <input type={type} id={label} name={name} onChange={onChange} value={value} />}
+			{type === "checkbox" ? (
+				<input
+					type={type}
+					id={label}
+					name={name}
+					placeholder={placeholder}
+					onChange={onChange}
+					value={value}
+					checked={checked}
+				/>
+			) : (
+				<input
+					type={type}
+					id={label}
+					name={name}
+					onChange={onChange}
+					value={value}
+					className={className}
+				/>
+			)}
 		</FormGroup>
 	);
 }
