a8f9d8b608cc00c6981eb842d2a1c5b7cf2687c5
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-29 12:24:53 +0530

    modal styles and UI updated

--- src/components/admin/dashboard/d-ui/Modal.jsx
+++ src/components/admin/dashboard/d-ui/Modal.jsx
@@ -4,7 +4,9 @@ import styles from "./Modal.module.css";
 export default function Modal({ children, onClose }) {
 	return createPortal(
 		<div className={styles.overlay} onClick={onClose}>
-			<div className={styles.modal}>{children}</div>
+			<div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+				{children}
+			</div>
 		</div>,
 		document.getElementById("modal")
 	);
