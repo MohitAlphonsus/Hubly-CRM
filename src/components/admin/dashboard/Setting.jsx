57d9936955325bd670fb13a08aeea7c4d48530da
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-29 13:16:53 +0530

    Team Edit Form functionality added, to edit and update team member information. Setting page updated

--- src/components/admin/dashboard/Setting.jsx
+++ src/components/admin/dashboard/Setting.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import { Button } from "../../ui";
 import { useTeamAuth } from "../../../hooks/useTeamAuth";
-import TeamForm from "./d-ui/TeamForm";
+import { useTeam } from "../../../hooks/useTeam";
+import TeamEditForm from "./d-ui/TeamEditForm";
+import { useParams } from "react-router";
 
 import styles from "./Setting.module.css";
 export default function Setting() {
 	const { logout } = useTeamAuth();
+	const { teamMember, handleFetchTeamMember } = useTeam();
+
+	const { id } = useParams();
+
+	useEffect(() => {
+		handleFetchTeamMember(id);
+	}, []);
+
 	return (
 		<div className={styles.setting}>
 			<Button
@@ -17,7 +28,7 @@ export default function Setting() {
 			</Button>
 			<div className={styles.edit__form__container}>
 				<h4>Edit Profile</h4>
-				<TeamForm />
+				<TeamEditForm teamMember={teamMember} />
 			</div>
 		</div>
 	);
---
+++ src/components/admin/dashboard/d-ui/TeamEditForm.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import { Input } from "../../../form";
+import styles from "./TeamEditForm.module.css";
+import { Button } from "../../../ui";
+import { useTeam } from "../../../../hooks/useTeam";
+
+export default function TeamEditForm({ teamMember }) {
+	const [formData, setFormData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+	const { handleEditTeamMember } = useTeam();
+
+	useEffect(() => {
+		setFormData({
+			firstName: teamMember.firstName || "",
+			lastName: teamMember.lastName || "",
+			email: teamMember.email || "",
+		});
+	}, []);
+
+	function handleChange(e) {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	}
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			if (formData.password !== formData.confirmPassword) {
+				throw new Error("Passwords do not match");
+			}
+
+			await handleEditTeamMember(teamMember._id, {
+				firstName: formData.firstName,
+				lastName: formData.lastName,
+				email: formData.email,
+				password: formData.password,
+			});
+
+			setFormData({
+				firstName: "",
+				lastName: "",
+				email: "",
+				password: "",
+				confirmPassword: "",
+			});
+
+			return alert("data updated successfully");
+		} catch (err) {
+			return alert(err.message);
+		}
+	}
+
+	return (
+		<div className={styles.team__form}>
+			<form onSubmit={handleSubmit}>
+				<Input
+					type="text"
+					label="First Name"
+					name="firstName"
+					placeholder="First Name"
+					value={formData.firstName}
+					onChange={handleChange}
+				/>
+				<Input
+					type="text"
+					label="Last Name"
+					name="lastName"
+					placeholder="Last Name"
+					value={formData.lastName}
+					onChange={handleChange}
+				/>
+				<Input
+					type="email"
+					label="Email"
+					name="email"
+					placeholder="Email"
+					value={formData.email}
+					onChange={handleChange}
+				/>
+				<Input
+					type="password"
+					label="Password"
+					name="password"
+					placeholder="********"
+					value={formData.password}
+					onChange={handleChange}
+				/>
+				<Input
+					type="password"
+					label="Confirm Password"
+					name="confirmPassword"
+					placeholder="********"
+					value={formData.confirmPassword}
+					onChange={handleChange}
+				/>
+
+				<Button type="submit" size="medium">
+					Save
+				</Button>
+			</form>
+		</div>
+	);
+}
